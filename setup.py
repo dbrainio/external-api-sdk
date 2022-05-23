4010a0942f2539a84a8cec81e858cb530721ee5e
Aynur Maksutov <maksutov_aynur@mail.ru>
2022-05-23 17:50:41 +0300

    change gateway

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ install_requires = [
 CONFIG = {
     'name': 'external-api-sdk',
     'url': 'https://github.com/dbrainio/external-api-sdk',
-    'version': '0.2.0',
+    'version': '0.4.0',
     'author': 's00ler',
     'install_requires': install_requires,
     'packages': find_packages()
